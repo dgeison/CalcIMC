482e4f741dc3103331a9583e41d71009bba0c2f5
dgeison <paradg@gmail.com>
2020-12-01 20:15:52 -0300

    Calculando adequadamente

--- calculomvc.js
+++ calculomvc.js
@@ -6,4 +6,11 @@ const sexo = "M"
 
 const imc = peso / (altura * altura)
 
+if(imc >= 30){
+    console.log(`O ${nome} está acima do peso`)
+}else{
+    console.log(`O ${nome} está abaixo do peso`)
+}
+
 console.log(imc) 
+
